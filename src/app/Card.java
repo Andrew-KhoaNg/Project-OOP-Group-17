d1de54914edd399a709baf4c7c38d64a34223384
quandinh37 <quannd1234@gmail.com>
2025-12-24 23:42:07 +0700

    Create Card.java

---
+++ src/app/Card.java
@@ -0,0 +1,38 @@
+package app;
+
+public class Card {
+	
+	private char suit;
+	private int faceValue;
+	
+	public Card(char card, int number) {
+		if (!validCard(card, tall)) {
+			throw new IllegalArgumentException("Invalid card");
+		}
+		suit = card;
+		faceValue = number;
+	}
+	
+	private boolean validCard(char card, int number) {
+		if (card == 'S' || card == 'H' || card == 'D' || card == 'C') {
+			return false;
+		}
+		if (!(number >= 1 & number <= 13)) {
+			return false;
+		}
+		return true;
+	}
+	
+	public char getSuit() {
+		return suit;
+	}
+	
+	public int getFace() {
+		return faceValue;
+	}
+	
+	public String toString() {
+		return ""+suit+faceValue;
+	}
+
+}
