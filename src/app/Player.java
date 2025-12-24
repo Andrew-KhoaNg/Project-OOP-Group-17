c366d0e1472cee1ca4ba4eac0daaa434884b2a12
Nguyen Thanh Dat <thanhdat06082006@gmail.com>
2025-12-24 23:32:30 +0700

    Update Player.java

--- src/app/Player.java
+++ src/app/Player.java
@@ -1,22 +1,35 @@
 package app;
 
 public class Player { 
-
-     private int money; 
-     private Card card1, card2; 
-     
-     public Player(int money, Card card1, Card card2) {
-         this.money = money;
-         this.card1 = card1;
-         this.card2 = card2;
-     }
-
-     public int getMoney() { return money; }
-     public void setMoney(int money) { this.money = money; }
-
-     public Card getCard1() { return card1; }
-     public void setCard1(Card card1) { this.card1 = card1; }
-
-     public Card getCard2() { return card2; }
-     public void setCard2(Card card2) { this.card2 = card2; }
+    
+    private int money; 
+    private Card card1, card2; 
+    
+    // Getter và Setter cho money
+    public int getMoney() {
+       return money;
+    }
+    public void setMoney(int money) {
+       this.money = money;
+    }
+    // Getter và Setter cho card1
+    public Card getCard1() {
+       return card1;
+    }
+    public void setCard1(Card card1) {
+       this.card1 = card1;
+    }
+    // Getter và Setter cho card2
+    public Card getCard2() {
+       return card2;
+    }
+    public void setCard2(Card card2) {
+       this.card2 = card2;
+    }
+    // Constructor
+    public Player(int money, Card card1, Card card2) {
+       this.money = money;
+       this.card1 = card1;
+       this.card2 = card2;
+    }
 }
