0a48a95c6dbbd2c836e14deeecc9516bdf8df302
hungbill2006-jpg <hungbill2006@gmail.com>
2025-12-25 14:07:39 +0700

    Create BlackjackObjectloader.java

---
+++ src/app/BlackjackObjectloader.java
@@ -0,0 +1,22 @@
+package app;
+
+public class BlackjackObjectloader {
+	
+	private int money;
+	private String identifier;
+	
+	public BlackjackObjectloader(String identifier, int money) {
+		this.money = this.money;
+		this.identifier = identifier;
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+	public String getIdentifier() {
+		return identifier;
+	}
+	
+
+}
